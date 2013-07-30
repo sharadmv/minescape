57e6b85e5a5b51ab193116f025a515df8ca855fd
Gerald Fong <geraldgfong@gmail.com>
2013-07-30 01:05:26 -0700

    Created shell methods for player movements

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'em-websocket'
+require 'json'
 
 EM.run do
   port = 9091
@@ -11,9 +12,36 @@ EM.run do
 
     ws.onclose { puts "Connection closed" }
 
-    ws.onmessage do |msg|
-      puts "Recieved message: #{msg}"
-      ws.send("Pong: #{msg}")
+    ws.onmessage do |raw_msg|
+      puts "Recieved message: #{raw_msg}"
+
+      msg = JSON.parse(msg)
+      command = msg['command']
+      player_id = msg['player_id']
+
+      player = Player[player_id] if player_id
+
+      case command
+      when 'player.forward_start'
+        player.forward_start unless player
+      when 'player.forward_stop'
+        player.forward_stop unless player
+      when 'player.backward_start'
+        player.backward_start unless player
+      when 'player.backward_stop'
+        player.backward_stop unless player
+      when 'player.strafe_left_start'
+        player.strafe_left_start unless player
+      when 'player.strafe_left_stop'
+        player.strafe_left_stop unless player
+      when 'player.strafe_right_start'
+        player.strafe_right_start unless player
+      when 'player.strafe_right_stop'
+        player.strafe_right_stop unless player
+      else
+        ws.send("Unknown command: #{command}")
+      end
+      ws.send("Performed command: #{command}")
     end
   end
 end
