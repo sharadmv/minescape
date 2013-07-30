e8404922319700bc4cebf49e380e7fca1b9f4e36
Gerald Fong <geraldgfong@gmail.com>
2013-07-30 01:46:09 -0700

    Added better routing to the server method for the player methods

--- server.rb
+++ server.rb
@@ -1,5 +1,8 @@
 require 'em-websocket'
 require 'json'
+require './util.rb'
+require './model/player.rb'
+
 
 EM.run do
   port = 9091
@@ -15,32 +18,41 @@ EM.run do
     ws.onmessage do |raw_msg|
       puts "Recieved message: #{raw_msg}"
 
-      msg = JSON.parse(msg)
-      command = msg['command']
-      player_id = msg['player_id']
-
-      player = Player[player_id] if player_id
-
-      case command
-      when 'player.forward_start'
-        player.forward_start unless player
-      when 'player.forward_stop'
-        player.forward_stop unless player
-      when 'player.backward_start'
-        player.backward_start unless player
-      when 'player.backward_stop'
-        player.backward_stop unless player
-      when 'player.strafe_left_start'
-        player.strafe_left_start unless player
-      when 'player.strafe_left_stop'
-        player.strafe_left_stop unless player
-      when 'player.strafe_right_start'
-        player.strafe_right_start unless player
-      when 'player.strafe_right_stop'
-        player.strafe_right_stop unless player
-      else
-        ws.send("Unknown command: #{command}")
+      begin
+        msg = JSON.parse(raw_msg)
+      rescue JSON::ParserError
+        ws.send("Malformed json: #{raw_msg}")
+        next
       end
+      command = msg['command']
+
+      object, method = command.split('.')
+
+      case object
+      when 'player'
+        if !msg.include?('player_id')
+          ws.send("Missing player_id")
+          next
+        end
+        player_id = msg['player_id']
+        player = Player[player_id]
+        
+        if player.nil?
+          ws.send("Invalid player_id #{player_id}")
+          next
+        end
+
+        if !Player.method_defined?(method.to_sym)
+          ws.send("Invalid method #{method}")
+          next
+        end
+
+        player.send(method.to_sym)
+      else
+        ws.send("Unknown object: #{object}")
+        next
+      end
+
       ws.send("Performed command: #{command}")
     end
   end
