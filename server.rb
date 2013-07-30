55259403b0c4439faf5f55d95fa746c8c6ad5821
Sharad Vikram <sharadmv@fb.com>
2013-07-29 22:19:27 -0700

    moar style fixes

--- server.rb
+++ server.rb
@@ -4,16 +4,16 @@ EM.run do
   port = 9091
   puts "Starting websocket server"
   EM::WebSocket.run(:host => "0.0.0.0", :port => port) do |ws|
-    ws.onopen { |handshake|
+    ws.onopen do |handshake|
       puts "WebSocket connection open"
       ws.send("Hello Client, you connected to #{handshake.path}")
-    }
+    end
 
     ws.onclose { puts "Connection closed" }
 
-    ws.onmessage { |msg|
+    ws.onmessage do |msg|
       puts "Recieved message: #{msg}"
       ws.send("Pong: #{msg}")
-    }
+    end
   end
 end
