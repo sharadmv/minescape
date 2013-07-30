dd3dc6355cf00391aca0c4ffbccad5457724566a
Gerald Fong <geraldgfong@gmail.com>
2013-07-30 00:21:17 -0700

    Remove the config file from git and load db to global namespace.

--- model/player.rb
+++ model/player.rb
@@ -1,5 +1,4 @@
 require 'sequel'
 
-DB = Sequel.connect('postgres://geraldfong:@localhost:5432/postgres')
 class Player < Sequel::Model
 end
--- util.rb
+++ util.rb
@@ -2,19 +2,10 @@ require 'yaml'
 require 'sequel'
 
 module Util
-  @@db = nil
-
-  def self.db
-    load_db unless @@db
-    @@db
-  end
-
-  def self.load_db
-    @@db = Sequel.connect(database_string)
-  end
-
   def self.database_string
     conf = YAML.load_file('./config.yml')['db']
     "postgres://#{conf['user']}:#{conf['password']}@#{conf['host']}:#{conf['port']}/postgres"
   end
 end
+
+DB = Sequel.connect(Util.database_string)
