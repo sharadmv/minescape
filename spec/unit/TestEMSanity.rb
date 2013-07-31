f83d9d45598dc6d910ac45ce0653cf7e5b1e5f3f
Sharad Vikram <sharadmv@fb.com>
2013-07-31 14:33:25 -0700

    fixed merge

--- spec/unit/TestEMSanity.rb
+++ spec/unit/TestEMSanity.rb
@@ -5,16 +5,9 @@ require './lib/GameServer.rb'
 describe GameServer do
   before do
     @gs = GameServer.new(9090)
-<<<<<<< HEAD
     @gs.start
   end
   it 'should be running' do
     @gs.isRunning.should(be_true)
-=======
-    @gs.start()
-  end
-  it 'should be running' do
-    @gs.isRunning().should be_true
->>>>>>> added in new testing framework
   end
 end
